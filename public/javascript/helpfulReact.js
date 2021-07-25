5c07f2b74d7d9a147a8bf185a8e8f77913ff8673
Sami Islam <paperchief@gmail.com>
2021-07-25 23:55:29 +0600

    front-end of home page

---
+++ public/javascript/helpfulReact.js
@@ -0,0 +1,12 @@
+$('.btn-counter').on('click', function(event, count) {
+  event.preventDefault();
+
+  var $this = $(this),
+      count = $this.attr('data-count'),
+      active = $this.hasClass('active'),
+      multiple = $this.hasClass('multiple-count');
+
+  $.fn.noop = $.noop;
+  $this.attr('data-count', ! active || multiple ? ++count : --count  )[multiple ? 'noop' : 'toggleClass']('active');
+
+});
